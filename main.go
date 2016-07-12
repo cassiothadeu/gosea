71db4e7b94db2a6e0ff2919fe741158a31df2652
Ross Nanopoulos <ross@komand.com>
2016-07-12 19:16:40 -0400

    Change to http.Handle and add middleware to users handler

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/komand/gosea/services"
 )
 
 func main() {
@@ -10,14 +12,23 @@ func main() {
 	keyPath := "server.key"
 	api := NewAPI(certPath, keyPath)
 
-	http.HandleFunc("/hello", api.Hello.ServeHTTP)
-	http.HandleFunc("/tokens", api.Tokens.ServeHTTP)
+	http.Handle("/hello", api.Hello)
+	http.Handle("/tokens", api.Tokens)
 
-	// TODO: Wrap with authentication and authorization middleware
-	http.HandleFunc("/users", api.Users.ServeHTTP)
+	http.Handle("/users", AddMiddleware(api.Users,
+		api.Authenticate,
+		api.Authorize(services.Permission("user_modify"))))
 
 	err := http.ListenAndServeTLS(":3000", certPath, keyPath, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+// AddMiddleware adds middleware to a Handler
+func AddMiddleware(h http.Handler, middleware ...func(http.Handler) http.Handler) http.Handler {
+	for _, mw := range middleware {
+		h = mw(h)
+	}
+	return h
 }
